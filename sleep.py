26a71c2f267a0978076d5ccd4c7a7983ad3c084a
AK-Bhatnagar <94451497+AK-Bhatnagar@users.noreply.github.com>
2022-06-15 17:10:30 +0530

    Update sleep.py

--- sleep.py
+++ sleep.py
@@ -1,2 +1,5 @@
-import time
-time.sleep(5)
+printf("Sleeping in public repo")
+printf("Sleeping in public repo")
+printf("Sleeping in public repo")
+printf("Sleeping in public repo")
+printf("Sleeping in public repo")
